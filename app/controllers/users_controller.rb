79f08faae18550cf88d0c7cb37272ff8e9b79d6b
masafumiog <gurasum041219@gmail.com>
2020-08-27 11:30:03 +0900

    ユーザー編集機能の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    current_user.update(user_params)
+  else
+    render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
 end
+
+# resourcesメソッド .railsで定義されている7つのアクションのルーティングを自動で生成するメソッド
+# onlyオプションを使うことで必要なアクションのみ生成させる
