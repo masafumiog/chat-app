cd7c6862e6d3650c1d6d1d42ea507f2883cb7778
masafumiog <gurasum041219@gmail.com>
2020-08-27 22:48:24 +0900

    messagesテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
+
 
 # resourcesメソッド .railsで定義されている7つのアクションのルーティングを自動で生成するメソッド
 # onlyオプションを使うことで必要なアクションのみ生成させる
