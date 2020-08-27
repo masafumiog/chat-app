6aa5f48f406b47511f1d8546f20c16964bb8d9bf
masafumiog <gurasum041219@gmail.com>
2020-08-27 15:53:01 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
 
 # resourcesメソッド .railsで定義されている7つのアクションのルーティングを自動で生成するメソッド
